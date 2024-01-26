68a6c61f398b170d11be9a2f70f6906d33f6c8ee
David I. Samudio <luminaxster@gmail.com>
2024-01-25 19:52:44 -0500

    missing example

---
+++ java/gmu/cs211/2024/spring/in-class/code/week01/section04/A.java
@@ -0,0 +1,14 @@
+class A{
+	public int f(){
+		return 1;
+	}
+
+	public static void main (String[] args){
+		int x = 7;
+		System.out.println("x: " +x);
+		System.out.println("x==7: " +(x==7));
+		String a = "";
+		String b = "";
+		System.out.println("a==b: " +(a==b));
+	}
+}
