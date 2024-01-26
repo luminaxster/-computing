7a288b6cc8d00eee31076084bdff6af7d32a95c6
David I. Samudio <luminaxster@gmail.com>
2024-01-25 20:10:06 -0500

    Fixes conflicted debug configs

--- java/gmu/cs211/2024/spring/in-class/code/week02/section03/DataLooper.java
+++ java/gmu/cs211/2024/spring/in-class/code/week02/section03/DataLooper.java
@@ -1,7 +1,7 @@
 // DO NOT SUBMIT CODE WITH IMPORTS, IT WILL DEDUCT POINTS DURING GRADING.
 // THIS IMPORT HIS HELPFUL TO DEBUG YOUR PROGRAM STATE
 import java.util.Arrays;
-public class DataLoop{
+public class DataLooper{
     public static void main(String[] args){
         System.out.println("Program start");
         int[] array = {7,49,350-7, 4, 6, 7, 0, 99, 100};
