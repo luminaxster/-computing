5d6e8d4989b056c7e0761cb27d7c3484546529eb
David I Samudio <luminaxster@gmail.com>
2024-02-15 17:05:22 -0500

    Create Cat.java

---
+++ java/gmu/cs211/2024/spring/in-class/code/week05/Cat.java
@@ -0,0 +1,16 @@
+// public class Cat extends Animal {
+//   Cat(String name){
+//   super(name);
+//   }
+  
+//   void meow(){}
+  
+//   // public static void main(String[] args) {
+//   //   Animal a =new Cat("A");
+//   //   a.exist();
+//   //   // a.meow(); // sub
+//   //   Cat b =new Cat("A");
+//   //   b.exist();// super
+//   //   b.meow(); // sub
+//   // }
+// }
